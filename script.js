955c5bc1cd84ad7df6f8d17980bd00f9d5e0d574
arditiadeta <arditiadeta@gmail.com>
2025-09-29 12:58:01 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function hitung(operator) {
+  let angka1 = parseFloat(document.getElementById("angka1").value);
+  let angka2 = parseFloat(document.getElementById("angka2").value);
+  let hasil = 0;
+
+  if (isNaN(angka1) || isNaN(angka2)) {
+    alert("Harap masukkan kedua angka!");
+    return;
+  }
+
+  switch (operator) {
+    case '+':
+      hasil = angka1 + angka2;
+      break;
+    case '-':
+      hasil = angka1 - angka2;
+      break;
+    case '*':
+      hasil = angka1 * angka2;
+      break;
+    case '/':
+      hasil = angka2 !== 0 ? angka1 / angka2 : "Tidak bisa dibagi 0";
+      break;
+  }
+
+  document.getElementById("output").innerText = hasil;
+}
